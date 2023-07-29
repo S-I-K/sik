b2a949616eb5375016645d6b3ab9f9f95970bd2e
SIK <daesik99@naver.com>
2023-07-30 01:27:32 +0900

    add: header, intro, about

--- js/main.js
+++ js/main.js
@@ -2,12 +2,30 @@ console.log(' 2023.07.11 ');
 
 /* jquery */
 $(()=>{
-    /* fullpage.js */
-    $('#fullpage').fullpage({
-		//options here
-		autoScrolling:true,
+  /* fullpage.js */
+  $('#fullpage').fullpage({
+
+    autoScrolling:true,
 		scrollHorizontally: true,
-        navigation: true,
+    navigation: true,
+    'afterLoad': function (anchorLink, index) {
+			if (index == 2){
+				$('header').css({
+          height: '89px',
+        });
+        $('header > .logo-area').css({
+          fontSize: '40px', 
+        });
+			}else{
+        $('header').css({
+          height: '100vh',
+        });
+        $('header > .logo-area').css({
+          fontSize: '68px',
+        });
+      }
+		},
+
 	});
 });
 
