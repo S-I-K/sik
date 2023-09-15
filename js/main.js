3975d96f410b9854a4c1334b7a64cc5075317f12
sik <daesik99@naver.com>
2023-09-15 18:01:40 +0900

    update/ work section

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 console.log(' 2023.07.11 ');
 
-/**************** jquery ****************/
+/**** jquery ****/
 $(()=>{
+
+
   /* fullpage.js */
   $('#fullpage').fullpage({
     autoScrolling:true,
@@ -28,14 +30,17 @@ $(()=>{
 		},
 	});
 
+
   /* real timer */
   let timer = $('#timer');
+  let year_txt = $('#year-txt');
   let day_txt = $('#day-txt');
   let month_txt = $('#month-txt');
   let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
   function getTime() {
     const date = new Date();
+    const year = String(date.getFullYear()).padStart(2, '0');
     const hours = String(date.getHours()).padStart(2, '0');
     const min = String(date.getMinutes()).padStart(2, '0');
     const sec = String(date.getSeconds()).padStart(2, '0');
@@ -43,14 +48,38 @@ $(()=>{
     const month = date.getMonth();
 
     timer.text(`${hours}:${min}:${sec}`);
+    year_txt.text(`${year}`);
     day_txt.text(`${day}`);
     month_txt.text(`${months[month]}`);
   }
   getTime();
   setInterval(getTime, 1000);
+
+
+  /* tab menu func */
+  let work_categorys = $('.category-box > ul li');
+
+  work_categorys.click(function(e){
+    let work_slide_boxs = $('.work-slide-box');
+    let work_slide_box = $('.work-slide-box').eq($(this).index());
+    console.log($(this).index());
+    console.log(work_slide_box);
+
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    work_slide_boxs.removeClass('active');
+    work_slide_box.addClass('active');
+  });
 });
 
-/**************** javascript ****************/
+
+
+
+
+
+
+
+/**** javascript ****/
 var time_weather_swiper = new Swiper("#time-weather-slider", {
   direction: 'vertical',
   autoplay: {
@@ -80,7 +109,7 @@ var hobby_swiper = new Swiper("#hobby-slider", {
   },
 });
 
-var swiper = new Swiper("#web-slider", {
+var swiper = new Swiper("#work-slider", {
     slidesPerView: 'auto',
     spaceBetween: 40,
     centeredSlides: true,
