b6b7634698629f999d41e6838e3a3d8cee0d5535
sik <daesik99@naver.com>
2024-03-27 11:54:54 +0900

    modified: header menu text effect

--- common/js/main.js
+++ common/js/main.js
@@ -1,5 +1,34 @@
-/* button effect */
-$button_effect = $('.button-effect');
-$button_effect.each(function(){
-    $(this).attr('data-hover', $(this).text());
+$(()=>{
+    /* button effect */
+    $button_effect = $('.button-effect');
+    $button_effect.each(function(){
+        $(this).attr('data-hover', $(this).text());
+    });
+
+
+
+    /* header commons */
+    $('.burger-wrap').mouseenter(()=>{
+        $('.burger-box span').css({
+            'top': '10px',
+        });
+    })
+    .mouseleave(()=>{
+        $('.burger-box span:nth-child(1)').css({
+            'top': '6px',
+        });
+        $('.burger-box span:nth-child(2)').css({
+            'top': '14px',
+        });
+    });
+
+    $('.burger-wrap').click(function(e){
+        e.preventDefault();
+
+        if(!$('.burger-box').hasClass('active')){
+            $('.burger-box').addClass('active');
+        }else{
+            $('.burger-box').removeClass('active');
+        }
+    });
 });
